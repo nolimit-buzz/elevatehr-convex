3debc37029869732c189075bf6dc550667cd51c4
Chijioke Udokporo <chijithemizzi@yahoo.co.uk>
2026-02-25 13:11:03 +0100

    Add Dockerfile and .dockerignore, update next.config.js and package.json formatting

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "standalone",
   reactStrictMode: true,
   images: {
-    domains: ["localhost", "127.0.0.1", "app.elevatehr.ai","api-convexy.codesordinatestudio.net.ng"],
+    domains: ["localhost", "127.0.0.1", "app.elevatehr.ai", "api-convexy.codesordinatestudio.net.ng"],
   },
 };
 
